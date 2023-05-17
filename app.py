a7ef879e2be6cb44f87db09861dd510545f3773d
Peter <peter@frog.net>
2023-05-17 10:05:07 +0100

    feat - search bar functioanlity set up and displays filtered data on page

--- app.py
+++ app.py
@@ -1,18 +1,43 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask_assets import Bundle, Environment
+
+from todo import todos
 
 app = Flask(__name__)
 
-# assets = Environment(app)
-# css = Bundle("src/main.css", output="dist/main.css")
+assets = Environment(app)
+js = Bundle("src/*.js", output="dist/main.js")
 
-# assets.register("css", css)
-# css.build()
+assets.register("js", js)
+js.build()
 
 # Sets base root to index.html
+
+
 @app.route("/")
 def main():
     return render_template('index.html')
 
+# Setup search endpoint
+
+
+@app.route("/search", methods=["POST"])
+def search_todo():
+    search_term = request.form.get("search")
+    if not len(search_term):
+        return render_template("todo.html", todos=[])
+
+    res_todos = []
+    for todo in todos:
+        if search_term in todo["title"]:
+            res_todos.append(todo)
+        elif search_term == str(todo["id"]):
+            res_todos.append(todo)
+        elif search_term == str(todo["completed"]).lower():
+            res_todos.append(todo)
+
+    return render_template("todo.html", todos=res_todos)
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
